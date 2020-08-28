61090bd67d06b4273f640d063566168f40dd0efe
barbaragribeiro <barbaragomes@dcc.ufmg.br>
2020-08-28 01:22:06 -0300

    Optimization + add channel info

--- scripts/filter_comments.py
+++ scripts/filter_comments.py
@@ -2,13 +2,11 @@
 import sys
 import os
 import json
-import csv
 import pandas as pd
 
 #Files are expected to be in the format .comments.jsonl
 
 COLUMNS = ['cid', 'text', 'time', 'author', 'channel', 'votes', 'video_id']
-ORIGINAL_COLUMNS = ['cid', 'text', 'time', 'author', 'channel', 'votes', 'photo', 'video_id']
 
 def toInt(n):
     n = n.replace(',', '.')
@@ -18,6 +16,19 @@ def toInt(n):
         return int(float(n[:-3])*1000000)
     else:
         return int(n)
+
+def dicts_generator(folder):
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith('.comments.jsonl'):
+                print('Reading ' + file)
+                video_id = file.split('.')[0]
+                with open(os.path.join(root,file)) as json_file:
+                    for line in json_file:
+                        dic = json.loads(line)
+                        dic['video_id'] = video_id
+                        del dic['photo']
+                        yield(dic)
 
 def main():
     folder = sys.argv[1]
@@ -29,25 +40,13 @@ def main():
         if not os.path.exists(outdir):
             os.makedirs(outdir)
 
-    with open(output, 'a') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=ORIGINAL_COLUMNS)
-        writer.writeheader() #Add header to csv
-
-        for root, dirs, files in os.walk(folder):
-            for file in files:
-                if file.endswith('.comments.jsonl'):
-                    print('Reading ' + file)
-                    video_id = file.split('.')[0]
-                    with open(os.path.join(root,file)) as json_file:
-                        dicts = [dict(json.loads(line), video_id=video_id) for line in json_file]
-                    for dic in dicts:
-                        writer.writerow(dic)
+    comments_gen = dicts_generator(folder)
             
     #Post-processing
-    df = pd.read_csv(output, usecols=COLUMNS, dtype={'votes':'str'})
+    df = pd.DataFrame(comments_gen)
 
     #Number of votes is in the form '999', '3,9 mil', '1,2 mi'
-    df['votes'] = df['votes'].apply(lambda x : toInt(x)).astype('Int64')
+    df['votes'] = df['votes'].astype('str').apply(lambda x : toInt(x)).astype('Int64')
 
     #Extract parent id and add replies count
     df['parent_id'] = df['cid'].apply(lambda x : x.split('.')[0] if '.' in x else None)
@@ -55,12 +54,15 @@ def main():
                     .count()\
                     .rename('replies')\
                     .astype('Int64')
-    df = df.join(replies_count, how='left', on='cid').fillna(0)
+    df = df.join(replies_count, how='left', on='cid')
+    df = df.fillna({'replies_count':0})
 
-    #Add video title
-    videos = pd.read_csv(video_data, usecols=['id','title'])
+    #Add video and channel info
+    videos = pd.read_csv(video_data, usecols=['id','title','uploader','uploader_id'])
     videos = videos.rename(columns={'id': 'video_id', 'title' : 'video_title'})
     df = pd.merge(df, videos, how='left', on='video_id')
+
+    df.to_csv(output, index=False)
 
     #Sorting
     df.sort_values('votes', ascending=False, inplace=True)
